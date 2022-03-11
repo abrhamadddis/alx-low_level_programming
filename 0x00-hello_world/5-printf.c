bb07995455b48c228f91353206c03587ed168985
abrhamadddis <abrhamaddis@gmail.com>
2022-03-11 10:07:14 -0800

    6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pieceof art,\n");
+	return(0);
+}
