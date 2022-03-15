91452e4e7ec76548ea9634a2fd61a2f101537257
abrhamadddis <abrhamaddis@gmail.com>
2022-03-15 13:55:56 -0700

    task 2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <main.h>
 /**
  *main - Starting point
  *
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
 return (0);
 }
