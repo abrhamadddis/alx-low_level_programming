2ef69ddbb3195ff7120623daf1ba20ab24f21b0a
abrhamadddis <abrhamaddis@gmail.com>
2022-03-16 10:47:14 -0700

    addition of number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @x: int type number
+ *
+ * @y: int typee number
+ *
+ * Return: result of addition
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
