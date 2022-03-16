371033400f6f0b6a6e821bb2c9a7a3a5e197998e
abrhamadddis <abrhamaddis@gmail.com>
2022-03-16 01:02:19 -0700

    that tell if it's upcase or lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower(int c): checkes for lowercase character
+ *
+ * c: char type letter
+ *
+ * Return: i if c is lowecase and 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
