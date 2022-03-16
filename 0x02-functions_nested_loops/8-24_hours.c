34260427e3d6b326ce73f96d79d8c61addde7e54
abrhamadddis <abrhamaddis@gmail.com>
2022-03-16 04:16:46 -0700

    jack number

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,7 +26,7 @@ void jack_bauer(void)
 				{
 					_putchar(i + '0');
 					_putchar(j + '0');
-					_putchar(':')
+					_putchar(':');
 					_putchar(k + '0');
 					_putchar(l + '0');
 					_putchar('\n');
