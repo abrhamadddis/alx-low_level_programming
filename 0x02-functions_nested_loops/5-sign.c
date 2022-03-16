721bccc9068c0e28ffb0e821bb81a71ccaae0073
abrhamadddis <abrhamaddis@gmail.com>
2022-03-16 01:53:05 -0700

    postive or negatvie

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign -pritn the sign of the number
+ * @n: type int interger, can be negatvie or positive
+ * Description: print +, 0, or -depending on number, along with return
+ * Return: 1 if num is postive 0 if the num is 0 -1 if the num is negatvie
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1);
+	}
+	else
+	{
+		_putcahr('0');
+		return (0);
+	}
+}
+
