2636688464be2d030e0599143d88315455e1fc5c
abrhamadddis <abrhamaddis@gmail.com>
2022-03-17 01:57:15 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -tests function that prints if integer is positive of negagtive
+ * Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(1);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging is the process of finding and fixing errors in software that prevetnts it from running correctly. this are some projects related to debugging.
