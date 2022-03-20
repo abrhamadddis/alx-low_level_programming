750ccb12e92f507bc22dc272e68632d33225d473
abrhamadddis <abrhamaddis@gmail.com>
2022-03-20 02:45:13 -0700

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_line - print diagonal n times
+ *
+ * @n: assign to number of diagonal
+ *
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int l, m;
+
+	if (n > 0)
+	{
+		for (m = 1; m <= n; m++)
+	{
+		for (l = m; l > 1; l--)
+		{
+			_putchar(32);
+		}
+		_putchar('\\');
+	_putchar('\n');
+	}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
