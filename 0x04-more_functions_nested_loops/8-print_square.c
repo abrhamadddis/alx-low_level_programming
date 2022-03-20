2cc1a33432e30e8306bd35b56f0b5bd89fd90a11
abrhamadddis <abrhamaddis@gmail.com>
2022-03-20 03:19:50 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,9 +16,9 @@ void print_square(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				_Putchar('#');
 			}
