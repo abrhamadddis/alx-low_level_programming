418e87aa74cf02721d6e5e199ea536b84cf126ff
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-21 22:58:00 -0700

    print selected one

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ *
+ * @str: input string
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (str[i] = 0; str[i] != '\0'; i++)
+	{
+		int n;
+
+		n = i % 2;
+		if (n == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar(0);
+}
