175932db2fd18629c1ae23d247c2270dc0119623
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-22 10:46:06 -0700

    copy string from src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strepy - function that copies the string pointed by src
+ *
+ * @src: char pointer variable
+ *
+ * @dest: char pointer variable
+ *
+ * Return: a sequence of characters
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
