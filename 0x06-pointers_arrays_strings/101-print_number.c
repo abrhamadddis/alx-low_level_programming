b999ac6ceb338f8f2933d148206c9c6a736d1e53
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-24 05:49:10 -0700

    advanced task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,14 +1,35 @@
 #include "main.h"
-
 /**
- * print_number - a function that return n
+ * print_number - function that prints an integer
  *
- * @n: input integer
+ * @n: int type var
  *
- * Return: nothing
+ * Return: Nothing
  */
-
 void print_number(int n)
 {
-	_putchar(n);
+	int adjustor, sign;
+
+	adjustor = 1000000000;
+	sign = 1;
+	if (n > 0)
+	{
+		n = n * -1;
+		sign = sign * -1;
+	}
+	if (n != 0)
+	{
+		while (n / adjustor == 0)
+			adjustor = adjustor / 10;
+		if (sign == 1)
+			_putchar('-');
+		while (adjustor >= 1)
+		{
+			_putchar(-(n / adjustor) + '0');
+			n = n % adjustor;
+			adjustor = adjustor / 10;
+		}
+	}
+	else
+		_putchar('0');
 }
