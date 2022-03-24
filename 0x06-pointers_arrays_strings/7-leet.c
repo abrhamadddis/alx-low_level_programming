ca182820646c02fc1b42118cf43e4586cdd1d196
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-24 04:50:30 -0700

    advanced task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,21 +7,16 @@
  */
 char *leet(char *s)
 {
-	int i, j;
-
-	i = 0;
+	int i = 0, j;
 	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	char str[10] = {'a', 'A', 'e', 'E', 'o', 'o', 't', 'T', 'l', 'L'};
+	char str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
-		{
 			if (s[i] == str[j])
-			{
 				s[i] = subs[j];
-			}
-		}
+
 		i++;
 	}
 
