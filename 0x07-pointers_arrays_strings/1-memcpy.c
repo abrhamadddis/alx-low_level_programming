9077fc798264c5081805bdeeb715c0df6a1637bc
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-28 05:09:58 -0700

    mandatory task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -18,7 +18,9 @@ char *_memcpy(char *dest, char src, unsigned int n)
 	pointer = dest;
 	for (i = 0; i <= n; i++)
 	{
-		*dest[i] = *src[i];
+		*dest = *src;
+		dest++;
+		src;
 	}
 	return (pointer);
 }
