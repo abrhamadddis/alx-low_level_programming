9385b1ae6aa45262ce8216ade9503d057e172c76
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-30 03:52:00 -0700

    mandatory task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - reverse the input string
- *
- * @s: input string
- *
- * Return: nothing
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
-	else
-	{
-		_print_rev_recursion(s + 1);
-	}
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
