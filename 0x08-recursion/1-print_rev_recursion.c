441aac89a70000c053daf76d1319ff25cbce2a29
abrhamadddis <abrhamaddis32@gmail.com>
2022-03-30 03:47:56 -0700

    mandatory task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,11 +11,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_print_rev_recursion(s--);
+		_print_rev_recursion(s - 1);
 		_putchar(*s);
 	}
 	else
 	{
-		_print_rev_recursion(s++);
+		_print_rev_recursion(s + 1);
 	}
 }
