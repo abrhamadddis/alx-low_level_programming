0c80749023bdc7b223cce6d7996ff9bc5572d3e2
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-06 02:02:13 -0700

    mandatory task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a function returns a pointer to new string
+ *
+ * @str: input string
+ * 
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+
+	char *s;
+	i = 0;
+	j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	else if (str = 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (j < i)
+		{
+			s[j] = str[j];
+			j++;
+		}
+		return (s);
+	}
+}
+
