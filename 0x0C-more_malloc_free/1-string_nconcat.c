b9db4763f26837216f5ef1ee30a191b6f3182bc0
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-07 04:27:28 -0700

    mandatory task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,20 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = 0;
 	len2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (s1)
 	{
 		while (s1[len1])
 			len1++;
-	}
-	if (s2 == NULL)
-	{
-		x = malloc((len1 + 1) * sizeof(char));
-		for (i = 0; i < len1; i++)
-		{
-			x[i] = s2[2];
-			return (x);
-		}
-		break;
 	}
 	if (s2)
 	{
