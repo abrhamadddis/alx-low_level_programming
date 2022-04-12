3e1f6030bae482be74c342b3dc83946a4e94a086
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-12 02:36:40 -0700

    mandatory task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,15 +21,8 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Name: %s\n", d->name);
-	}
-	if (d->age == NULL)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
 		printf("Age: %f\n", d->age);
-	}
+	}	
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
