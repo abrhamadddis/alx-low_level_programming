cfcbb95d998055f80647f46312c34d2851e0b36b
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-12 02:30:08 -0700

    mandatory task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function that print struct dog
+ *
+ * @d: pinter to struct dog
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age == NULL)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
