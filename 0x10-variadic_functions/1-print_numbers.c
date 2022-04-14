b2f72394042de19197d3687acbddf45f758a1f3f
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-14 12:10:28 -0700

    mandatory task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,8 +21,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(string, int));
-		if (separator != NULL)
+		printf("%d",  va_arg(string, int));
+		if (separator != NULL && (i < (n - 1)))
 			printf("%s", separator);
 	}
 	va_end(string);
