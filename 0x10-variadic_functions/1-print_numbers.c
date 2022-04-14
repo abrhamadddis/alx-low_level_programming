77e157882fa2de9456a45448dd83c9422edd2460
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-14 12:06:32 -0700

    mandatory task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - variadic function to  print number
+ *
+ * @separator: const char type pointer to string between numbers
+ *
+ * @n: const unsigned int type
+ *
+ * Return: Always successful
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list string;
+
+	va_start(string, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(string, int));
+		if (separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(string);
+	printf("\n");
+}
