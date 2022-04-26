588460c5377cce10d13f15fec89d08968a10c04e
abrhamadddis <abrhamaddis32@gmail.com>
2022-04-26 01:03:52 -0700

    mandatory task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - function to free heap
+ *
+ * @head: pointer to struct of listint type
+ *
+ * Return: always successful
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = NULL;
+
+	while (head != NULL)
+	{
+		temp = head;
+		temp = temp->next;
+		free(temp);
+	}
+}
