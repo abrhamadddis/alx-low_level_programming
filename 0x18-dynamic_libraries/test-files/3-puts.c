d4187acbcbfed2491d74b3f696d948731933fc20
abrhamadddis <abrhamaddis32@gmail.com>
2022-06-13 12:05:58 -0700

    mandatory task

---
+++ 0x18-dynamic_libraries/test-files/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ * @s: char pointer variable
+ *
+ * Return: nothing
+ */
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar(10);
+}
