3c959fc33baa41b24923f7977586e38304fdb8ca
abrhamadddis <abrhamaddis32@gmail.com>
2022-10-05 21:01:59 +0300

    first task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,20 +8,19 @@
  *  Return: if the value is not present of the give array is null
  *          return -1
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+size_t i;
 
-    if (array == NULL)
-        return -1;
+if (array == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-    
-    }
-
-    return (-1);
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
 }
